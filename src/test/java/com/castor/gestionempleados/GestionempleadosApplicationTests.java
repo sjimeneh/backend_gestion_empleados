c4eca75794f28850146c7f8a54f40330b58f253a
sjimeneh <samuelhz1998@gmail.com>
2024-04-03 01:24:31 -0500

    ALL: Se inicia el proyecto, se establece la arquitectura de carpetas, y se configura el archivo application.yml

---
+++ src/test/java/com/castor/gestionempleados/GestionempleadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castor.gestionempleados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionempleadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
